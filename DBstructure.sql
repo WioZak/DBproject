c67fca8b98192e010e894e11a8a5ba92ee47de96
WioZak <wiolet.00@gmail.com>
2017-05-13 15:43:46 +0200

    add pacjent terapia relation

--- DBstructure.sql
+++ DBstructure.sql
@@ -4,20 +4,20 @@ CREATE DATABASE SZPITAL
 GO
 
 CREATE TABLE SZPITAL..lekarze
-    ( lekarz_id      INT PRIMARY KEY
-    , lekarz_imie    VARCHAR(25) 
-	, lekarz_nazwisko VARCHAR(50)
-	, lekarz_pensja MONEY
-	, lekarz_tytul VARCHAR(25)
-	, lekarz_specjalizacja VARCHAR(50)
-	, lekarz_pesel VARCHAR(11)
-	, oddzial_id INT
+    ( lekarz_id				INT PRIMARY KEY
+    , lekarz_imie			VARCHAR(25) 
+	, lekarz_nazwisko		VARCHAR(50)
+	, lekarz_pensja			MONEY
+	, lekarz_tytul			VARCHAR(25)
+	, lekarz_specjalizacja	VARCHAR(50)
+	, lekarz_pesel			VARCHAR(11)
+	, oddzial_id			INT
     );
 GO
 
 CREATE TABLE SZPITAL..oddzialy 
-    ( oddzial_id INT PRIMARY KEY
-	, oddzial_nazwa VARCHAR(50)
+    ( oddzial_id		INT PRIMARY KEY
+	, oddzial_nazwa		VARCHAR(50)
     ); 
 GO
 
@@ -26,26 +26,26 @@ GO
 
 
 CREATE TABLE SZPITAL..pacjenci
-    ( pacjent_id      INT PRIMARY KEY
-    , pacjent_imie    VARCHAR(25) 
-	, pacjent_nazwisko VARCHAR(50)
-	, pacjent_pesel VARCHAR(11) NOT NULL UNIQUE
+    ( pacjent_id				INT PRIMARY KEY
+    , pacjent_imie				VARCHAR(25) 
+	, pacjent_nazwisko			VARCHAR(50)
+	, pacjent_pesel				VARCHAR(11) NOT NULL UNIQUE
 	CONSTRAINT pacjent_peselC CHECK (pacjent_pesel LIKE '%[0-9]%')
-	, pacjent_data_przyjecia DATE NOT NULL
-	, pacjent_data_wypisu DATE
-	, lekarz_id INT NOT NULL
-	, choroba_id INT
+	, pacjent_data_przyjecia	DATE NOT NULL
+	, pacjent_data_wypisu		DATE
+	, lekarz_id					INT NOT NULL
+	, choroba_id				INT
+	, terapia_id				INT
     );
-GO
-
-
-ALTER TABLE SZPITAL..lekarze ADD CONSTRAINT oddzial_id_fk FOREIGN KEY (oddzial_id) REFERENCES oddzialy(oddzial_id);
 GO
 
 CREATE TABLE SZPITAL..jednostki_chorobowe
     ( choroba_id		INT PRIMARY KEY
     , choroba_nazwa		VARCHAR(50) NOT NULL
     );
+GO
+
+ALTER TABLE SZPITAL..pacjenci ADD CONSTRAINT lekarz_id_fk FOREIGN KEY (lekarz_id) REFERENCES lekarze(lekarz_id);
 GO
 
 ALTER TABLE SZPITAL..pacjenci ADD CONSTRAINT choroba_id_fk FOREIGN KEY (choroba_id) REFERENCES jednostki_chorobowe(choroba_id);
@@ -61,6 +61,9 @@ CREATE TABLE SZPITAL..terapie
     );
 GO
 
+ALTER TABLE SZPITAL..pacjenci ADD CONSTRAINT terapia_id_fk FOREIGN KEY (terapia_id) REFERENCES terapie(terapia_id);
+GO
+
 CREATE TABLE SZPITAL..zabiegi
     ( zabieg_id		INT PRIMARY KEY
     , zabieg_nazwa	VARCHAR(50)
@@ -69,9 +72,9 @@ CREATE TABLE SZPITAL..zabiegi
 GO
 
 CREATE TABLE SZPITAL..lekarstwa
-    ( lekarstwo_id		INT PRIMARY KEY
-    , lekarstwo_nazwa	VARCHAR(50)
-	, lekarstwo_opis	VARCHAR(200)
+    ( lekarstwo_id					INT PRIMARY KEY
+    , lekarstwo_nazwa				VARCHAR(50)
+	, lekarstwo_opis				VARCHAR(200)
 	, lekarstwo_dawkowanie_dzien	INT
     );
 GO
