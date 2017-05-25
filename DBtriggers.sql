b20bad5f60169d51b0fa1b0b73a53d8a56cfeda5
WioZak <wiolet.00@gmail.com>
2017-05-25 21:13:21 +0200

    first trigger

--- DBtriggers.sql
+++ DBtriggers.sql
@@ -1,5 +1,27 @@
 -- WYZWALACZE
 -- 1. Nie pozwól na usuniêcie niezakoñczonej terapii. Wyœwietl odpowiedni komunikat. Udowodnij dzia³anie wyzwalacza (przed/po).
+--DROP TRIGGER usuwanie_niezakonczonego_leczenia
+
+USE SZPITAL
+GO
+CREATE TRIGGER usuwanie_niezakonczonej_terapii
+ON szpital.dbo.historia_leczenia
+INSTEAD OF DELETE
+AS
+BEGIN
+DECLARE @dataZakonczenia DATE
+DECLARE @leczenieID INT
+
+SELECT @dataZakonczenia = deleted.leczenie_data_zak FROM deleted
+SELECT @leczenieID = deleted.leczenie_id FROM deleted
+
+	IF (@dataZakonczenia is NULL)
+		BEGIN
+			PRINT 'Nie mo¿na usun¹æ niezakoñczonej terapii. Nie usuniêto terapii o id  ' + CAST(@leczenieID AS VARCHAR)
+		END
+END
+
+DELETE FROM historia_leczenia WHERE leczenie_id = 34
 
 -- 2. Zabroñ zmiejszenia pensji lekarza. Wyœwietl komunikat. Udowodnij dzia³anie wyzwalacza (przed/po).
 
